ffc04b3e694943db047d09d6f2b5945febca9d2d
harshbhavsar <harsh.bhavsar@copart.com>
2017-07-09 04:17:13 -0500

    Spring Boot Changes

---
+++ src/main/java/com/unnat/krushi/UnnatKrushiPranaliApplication.java
@@ -0,0 +1,13 @@
+package com.unnat.krushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class UnnatKrushiPranaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnnatKrushiPranaliApplication.class, args);
+	}
+}
